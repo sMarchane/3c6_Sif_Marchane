9338ddcf3350e5bc7bb163c27545689d8725c66c
SIF MARCHANE <1922145cmontmorency.qc.ca>
2021-08-30 08:54:09 -0400

    atelier 1.3

---
+++ atelier1_3/src/atelier1_3/MonAtelier1_3.java
@@ -0,0 +1,62 @@
+package atelier1_3;
+
+import ateliers.atelier1_3.Atelier1_3;
+
+public class MonAtelier1_3 extends Atelier1_3{
+
+	@Override
+	public void methode01() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode02() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode03() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode04() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode05() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode06() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode07() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode08() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void methode09() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ tutoriel1_3/src/tutoriel1_3/MonTutoriel1_3.java
@@ -0,0 +1,22 @@
+package tutoriel1_3;
+
+import tutoriels.tutoriel1_3.Tutoriel1_3;
+import tutoriels.tutoriel1_3.Tutoriel1_3_v2;
+
+public class MonTutoriel1_3  extends Tutoriel1_3_v2{
+
+
+	@Override
+	public int fournirUnEntier() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double fournirUnReel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
